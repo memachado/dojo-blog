9670bc6a91a23ed04457c1269e1cdba65cd34fca
Miguel <20932346+memachado@users.noreply.github.com>
2023-10-20 22:07:16 -0300

    Lesson 7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const hancleClick = (e) => {
+        console.log('Hello World', e);
+    }
+
+    const hancleClickAgain = (name, e) => {
+        console.log(`Hello ${name}`, e.target);
+    }
+
     return (
         <div className="home">
             <h2>Home</h2>
+            <button onClick={hancleClick} type="button">Click Me</button>
+            <button onClick={(e) => hancleClickAgain('Marco!', e)}>Click Me Again</button>
         </div>
     );
 }
