66ebadb4e4283a2de70febf7ba7af0719e033a5a
Miguel <20932346+memachado@users.noreply.github.com>
2023-10-20 21:47:00 -0300

    Lesson 5

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
-  const title = "Welcome to the new blog!";
-  const likes = 50;
-
-  const link = 'https://www.google.com';
-
   return (
     <div className="App">
+
+      <Navbar />
+
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} likes</p>
 
-        <p>{ 10 }</p>
-        <p>{ 'hello ninjas' }</p>
-        <p>{ [1, 2, 3] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-
-        <a href={link}>Google Site</a>
-
+        <Home />
+      
       </div>
     </div>
   );
