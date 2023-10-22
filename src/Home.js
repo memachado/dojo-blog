472bb1de4a7ea9b382c616669151d91269413a72
Miguel <20932346+memachado@users.noreply.github.com>
2023-10-22 20:04:40 -0300

    Lesson 17.b

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,6 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
@@ -24,7 +19,7 @@ const Home = () => {
     return (
         <div className="home">
             
-            {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
+            {blogs && <BlogList blogs={blogs} title="All blogs" />}
             
         </div>
     );
