2dd9476db958a493bf5fa62f2c60eda7ce7d1ebb
Miguel <20932346+memachado@users.noreply.github.com>
2023-10-23 20:51:19 -0300

    Lesson 19

--- src/Home.js
+++ src/Home.js
@@ -7,21 +7,36 @@ const Home = () => {
 
     const [isPending, setIsPending] = useState(true);
 
+    const [error, setError] = useState(null);
+
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(res => {
-                    return res.json()
+
+                    if (!res.ok) {
+                        throw Error('could not fetch the data for that resource');
+                    }
+
+                    return res.json();
+
                 }).then(data => {
                     setBlogs(data);
                     setIsPending(false);
+                    setError(null);
                 })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                });
         }, 1000);
 
     }, []);
 
     return (
         <div className="home">
+
+            {error && <div>{error}</div>}
 
             {isPending && <div>Loading...</div>}
 
