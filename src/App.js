42d51ae7d1da8bcfc2b0f6f5cafb34a446380d2c
Miguel <20932346+memachado@users.noreply.github.com>
2023-10-23 21:19:08 -0300

    Lesson 22

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
 
@@ -15,8 +16,12 @@ function App() {
 
           <Switch>
 
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+
+            <Route path="/create">
+              <Create />
             </Route>
 
           </Switch>
